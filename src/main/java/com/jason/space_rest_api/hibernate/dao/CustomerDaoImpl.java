863f10ed77def5895ac3291899e8fbf7eefc2e13
jasonbwtan <jasonbwtan@hotmail.com>
2015-09-16 21:31:30 +0100

    initial commit

--- src/main/java/com/jason/space_rest_api/hibernate/dao/CustomerDaoImpl.java
+++ src/main/java/com/jason/space_rest_api/hibernate/dao/CustomerDaoImpl.java
@@ -1,7 +1,11 @@
 package com.jason.space_rest_api.hibernate.dao;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -9,12 +13,13 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 import com.jason.space_rest_api.hibernate.model.Customer;
+import com.microsoft.schemas.office.x2006.encryption.CTKeyEncryptor.Uri;
 
 public class CustomerDaoImpl implements CustomerDao<Customer, Long> {
 
 	private Session currentSession;
 	private Transaction currentTransaction;
-
+	static final Logger logger = LogManager.getLogger();
 	public CustomerDaoImpl() {
 	}
 
@@ -38,8 +43,24 @@ public class CustomerDaoImpl implements CustomerDao<Customer, Long> {
 		currentSession.close();
 	}
 	
-	private static SessionFactory getSessionFactory() {
+	private static SessionFactory getSessionFactory(){
 		Configuration configuration = new Configuration().configure();
+		String DATABASE_URL = System.getenv("DATABASE_URL");
+		if(DATABASE_URL != null && !DATABASE_URL.equals("")){
+			URI dbUri = null;
+			try {
+				dbUri = new URI(DATABASE_URL);
+			} catch (URISyntaxException e) {
+				logger.info("Could not read DATABASE_URL heroku environment variable");
+				e.printStackTrace();
+			}
+			configuration.setProperty("hibernate.connection.url", "jdbc:postgresql://"+dbUri.getHost()+":"+dbUri.getPort()+":"+dbUri.getPath());
+			configuration.setProperty("hibernate.connection.username", dbUri.getUserInfo().split(":")[0]);
+			configuration.setProperty("hibernate.connection.password", dbUri.getUserInfo().split(":")[1]);
+
+		} else {
+			logger.info("Using default hibernate.cfg.xml properties");
+		}
 		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
 				.applySettings(configuration.getProperties());
 		SessionFactory sessionFactory = configuration.buildSessionFactory(builder.build());
